92907546bd2228f06d7ae18cf230c3228d6685ff
hao_hu <278624508@qq.com>
2016-05-23 13:42:38 +0800

    U3d游戏开发

---
+++ SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float zMin, zMax, xMin, xMax;
+}
+public class PlayerController : MonoBehaviour {
+    public Rigidbody rb;
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
+
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+        rb.velocity = movement * speed;
+        rb.position = new Vector3
+            (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+            );
+
+        rb.rotation = Quaternion.Euler(0.0f,0.0f,rb.velocity.x * -tilt);
+	}
+}
